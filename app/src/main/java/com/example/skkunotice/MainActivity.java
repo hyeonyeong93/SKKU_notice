d645884525145e1e312fc75c9ae7a218583e1c4c
hyeonyeong93 <qpxhqpslll@gmail.com>
2020-01-26 20:18:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/skkunotice/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.skkunotice;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    // 알림생성버튼
+    private Button create;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        create = findViewById(R.id.create_alarm);
+
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(View view) {
+
+                createNotification();
+            }
+        });
+
+    }
+
+    private void createNotification() {
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+
+        // 알림 내용
+        builder.setContentTitle("소프트웨어대학 공지사항");
+        builder.setContentText("2020 인공지능 데이터셋 경진대회 안내");
+
+        builder.setColor(Color.GREEN);
+
+        // 알림 표시
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(new NotificationChannel("default", "기본 채널", NotificationManager.IMPORTANCE_DEFAULT));
+        }
+
+        // id값은
+        // 정의해야하는 각 알림의 고유한 int값
+        notificationManager.notify(1, builder.build());
+    }
+
+    private void removeNotification() {
+
+
+
+        // Notification 제거
+        NotificationManagerCompat.from(this).cancel(1);
+    }
+
+
+
+}
